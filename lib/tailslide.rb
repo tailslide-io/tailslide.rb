c07cdaba7c3d1f223045f3890802e0518b38e449
Steven Liou <stevenliou@gmail.com>
2022-07-13 17:27:51 -0400

    Initial commit

---
+++ lib/tailslide.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tailslide/version"
+
+module Tailslide
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tailslide/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tailslide
+  VERSION = "0.1.0"
+end
